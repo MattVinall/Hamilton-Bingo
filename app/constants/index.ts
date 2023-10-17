307aff3c0c2083b94d9ce426130ab454c2cdb54b
Matt Vinall <matt.vinall7@gmail.com>
2023-10-17 11:41:58 -0400

    businesses bingo objcet with all the categories for each letter

---
+++ app/constants/index.ts
@@ -0,0 +1,522 @@
+export const businesses = {
+  B: [
+    {
+      name: "The Cannon",
+      address: "Address",
+    },
+    {
+      name: "Crown & Press",
+      address: "Address",
+    },
+    {
+      name: "Rooney's'",
+      address: "Address",
+    },
+    {
+      name: "One For All",
+      address: "Address",
+    },
+    {
+      name: "Saint Jaimes",
+      address: "Address",
+    },
+    {
+      name: "Vintage",
+      address: "Address",
+    },
+    {
+      name: "Phin",
+      address: "Address",
+    },
+    {
+      name: "Paisley",
+      address: "Address",
+    },
+    {
+      name: "Cafe Domestiique",
+      address: "Address",
+    },
+    {
+      name: "Grandads Donuts",
+      address: "Address",
+    },
+    {
+      name: "Donut Monster ",
+      address: "Address",
+    },
+    {
+      name: "Synonym",
+      address: "Address",
+    },
+    {
+      name: "Detour Cafe",
+      address: "Address",
+    },
+    {
+      name: "Relay Coffee",
+      address: "Address",
+    },
+    {
+      name: "Smalls",
+      address: "Address",
+    },
+    {
+      name: "Redchurch",
+      address: "Address",
+    },
+    {
+      name: "Williams Pier 8",
+      address: "Address",
+    },
+    {
+      name: "Cafe Baffico",
+      address: "Address",
+    },
+    {
+      name: "Emerald Coffee",
+      address: "Address",
+    },
+  ],
+  I: [
+    {
+      name: "Gage Park",
+      address: "Address",
+    },
+    {
+      name: "Bayfront Park",
+      address: "Address",
+    },
+    {
+      name: "Art Gallery of Hamilton",
+      address: "Address",
+    },
+    {
+      name: "Dundas Driving Park",
+      address: "Address",
+    },
+    {
+      name: "Dundas Valley Conservation Area",
+      address: "Address",
+    },
+    {
+      name: "Bard & Bear",
+      address: "Address",
+    },
+    {
+      name: "Play With Clay",
+      address: "Address",
+    },
+    {
+      name: "Rivers & Meadows",
+      address: "Address",
+    },
+    {
+      name: "Hamilton Farmers Market",
+      address: "Address",
+    },
+    {
+      name: "Ottawa St Farmers Market",
+      address: "Address",
+    },
+    {
+      name: "Hamilton Public Library",
+      address: "Address",
+    },
+    {
+      name: "Escarpment Stairs",
+      address: "Address",
+    },
+    {
+      name: "Tim Hortons Field",
+      address: "Address",
+    },
+    {
+      name: "Skating at Pier 8",
+      address: "Address",
+    },
+    {
+      name: "Empire Make Room",
+      address: "Address",
+    },
+    {
+      name: "Planetarium (McMaster)",
+      address: "Address",
+    },
+    {
+      name: "Kenora Transfer Station",
+      address: "Address",
+    },
+    {
+      name: "RBG",
+      address: "Address",
+    },
+    {
+      name: "Theatre Aquarius",
+      address: "Address",
+    },
+    {
+      name: "Playhouse Cinema",
+      address: "Address",
+    },
+    {
+      name: "Wild Waterworks",
+      address: "Address",
+    },
+    {
+      name: "The Move Room",
+      address: "Address",
+    },
+    {
+      name: "Skyway Bowl",
+      address: "Address",
+    },
+    {
+      name: "Devils Punchbowl",
+      address: "Address",
+    },
+  ],
+  N: [
+    {
+      name: "Bernie's",
+      address: "Address",
+    },
+    {
+      name: "Shorty's",
+      address: "Address",
+    },
+    {
+      name: "Goldie's",
+      address: "Address",
+    },
+    {
+      name: "Artie's",
+      address: "Address",
+    },
+    {
+      name: "Flora",
+      address: "Address",
+    },
+    {
+      name: "Cowabunga",
+      address: "Address",
+    },
+    {
+      name: "Hearty Hooligan",
+      address: "Address",
+    },
+    {
+      name: "Castelli Cucina",
+      address: "Address",
+    },
+    {
+      name: "Capri Gelateria",
+      address: "Address",
+    },
+    {
+      name: "Pinch",
+      address: "Address",
+    },
+    {
+      name: "Shuck Truck",
+      address: "Address",
+    },
+    {
+      name: "McQueens",
+      address: "Address",
+    },
+    {
+      name: "Banh Mi Hue",
+      address: "Address",
+    },
+    {
+      name: "Rony's",
+      address: "Address",
+    },
+    {
+      name: "Maria's Tortas Jalisco",
+      address: "Address",
+    },
+    {
+      name: "Mikey's creampies",
+      address: "Address",
+    },
+    {
+      name: "1101",
+      address: "Address",
+    },
+    {
+      name: "Nations Hot Bar",
+      address: "Address",
+    },
+    {
+      name: "All the spots in Farmers Market",
+      address: "Address",
+    },
+    {
+      name: "Poke",
+      address: "Address",
+    },
+    {
+      name: "Burnt Tongue",
+      address: "Address",
+    },
+    {
+      name: "Verlan",
+      address: "Address",
+    },
+    {
+      name: "Motel",
+      address: "Address",
+    },
+    {
+      name: "Electric Diner",
+      address: "Address",
+    },
+    {
+      name: "Sagarmatha Curry",
+      address: "Address",
+    },
+    {
+      name: "BN",
+      address: "Address",
+    },
+    {
+      name: "Hutch's",
+      address: "Address",
+    },
+  ],
+  G: [
+    {
+      name: "ARK Collective",
+      address: "Address",
+    },
+    {
+      name: "Hamilton Antique Mall",
+      address: "Address",
+    },
+    {
+      name: "Drop Spot",
+      address: "Address",
+    },
+    {
+      name: "Snafu Studios",
+      address: "Address",
+    },
+    {
+      name: "Girl on the wing",
+      address: "Address",
+    },
+    {
+      name: "Studio 205",
+      address: "Address",
+    },
+    {
+      name: "Dundurn Market",
+      address: "Address",
+    },
+    {
+      name: "Ottawa Market",
+      address: "Address",
+    },
+    {
+      name: "Strathcona Market ",
+      address: "Address",
+    },
+    {
+      name: "East Hamilton Cheese",
+      address: "Address",
+    },
+    {
+      name: "Mickey McGuires Cheese",
+      address: "Address",
+    },
+    {
+      name: "Parkette",
+      address: "Address",
+    },
+    {
+      name: "Barton Lettuce",
+      address: "Address",
+    },
+    {
+      name: "Vintage Soul Geek",
+      address: "Address",
+    },
+    {
+      name: "Daddy's plastic",
+      address: "Address",
+    },
+    {
+      name: "Latin Foods",
+      address: "Address",
+    },
+    {
+      name: "Dear Grain",
+      address: "Address",
+    },
+    {
+      name: "Bichette",
+      address: "Address",
+    },
+    {
+      name: "Pretty Grit",
+      address: "Address",
+    },
+    {
+      name: "Wear's Garden Centre",
+      address: "Address",
+    },
+    {
+      name: "Handknit",
+      address: "Address",
+    },
+    {
+      name: "Needlework",
+      address: "Address",
+    },
+    {
+      name: "Nerdcore",
+      address: "Address",
+    },
+  ],
+  O: [
+    {
+      name: "Merit",
+      address: "Address",
+    },
+    {
+      name: "Endless",
+      address: "Address",
+    },
+    {
+      name: "Wildcat",
+      address: "Address",
+    },
+    {
+      name: "Mai Pai",
+      address: "Address",
+    },
+    {
+      name: "Merk",
+      address: "Address",
+    },
+    {
+      name: "Argyle",
+      address: "Address",
+    },
+    {
+      name: "Martello",
+      address: "Address",
+    },
+    {
+      name: "Berkely North",
+      address: "Address",
+    },
+    {
+      name: "Caro",
+      address: "Address",
+    },
+    {
+      name: "Born & Raised",
+      address: "Address",
+    },
+    {
+      name: "Odds",
+      address: "Address",
+    },
+    {
+      name: "The Brain",
+      address: "Address",
+    },
+    {
+      name: "Farside",
+      address: "Address",
+    },
+    {
+      name: "Pintoh",
+      address: "Address",
+    },
+    {
+      name: "Mule",
+      address: "Address",
+    },
+    {
+      name: "Berkeley North",
+      address: "Address",
+    },
+    {
+      name: "The French",
+      address: "Address",
+    },
+    {
+      name: "Undefined",
+      address: "Address",
+    },
+    {
+      name: "Mystic Ramen",
+      address: "Address",
+    },
+    {
+      name: "Diplomat",
+      address: "Address",
+    },
+    {
+      name: "Rapscallion",
+      address: "Address",
+    },
+    {
+      name: "Castelli Cucina",
+      address: "Address",
+    },
+    {
+      name: "Bon Temps",
+      address: "Address",
+    },
+    {
+      name: "Earth to Table / Bread Bar",
+      address: "Address",
+    },
+    {
+      name: "August 8",
+      address: "Address",
+    },
+    {
+      name: "Owl of Minerva",
+      address: "Address",
+    },
+    {
+      name: "Korea House",
+      address: "Address",
+    },
+    {
+      name: "Sushi on Fennel",
+      address: "Address",
+    },
+    {
+      name: "Bankok Spoon",
+      address: "Address",
+    },
+    {
+      name: "Fairweather",
+      address: "Address",
+    },
+    {
+      name: "Collective Arts",
+      address: "Address",
+    },
+    {
+      name: "Capitol Bar",
+      address: "Address",
+    },
+    {
+      name: "Radius",
+      address: "Address",
+    },
+    {
+      name: "Plank",
+      address: "Address",
+    },
+  ],
+};
+
+export const categories: string[] = ["B", "I", "N", "G", "O"];

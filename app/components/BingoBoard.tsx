e31ea8a2bebfede2037963286965aa08668a47b2
Matt Vinall <matt.vinall7@gmail.com>
2023-10-17 15:55:05 -0400

    remove filter

--- app/components/BingoBoard.tsx
+++ app/components/BingoBoard.tsx
@@ -27,9 +27,6 @@ const BingoBoard: React.FC<Props> = ({ businesses }) => {
                     <div key={key + index} className="col-span-1">
                         <h3 className="text-6xl text-center text-black font-bold mb-2">{key}</h3>
                         {bingoBoard[key as keyof BingoBoard]
-                            .filter((business: Business, index: number, array: Business[]) => {
-                                return array.findIndex((b) => b.id === business.id) === index;
-                            })
                             .map((business: Business) => (
                                 <div key={business.id} className="w-full flex flex-col items-center justify-center h-[50%] p-2 mr-4 mt-4 mb-4 rounded-lg bg-gray-900">
                                     <h4 className="text-sm font-bold mb-1">{business.name}</h4>

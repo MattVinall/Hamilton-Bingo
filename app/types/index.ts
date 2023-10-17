57fe29af06577475c6b68cb158a8ad827acdf691
Matt Vinall <matt.vinall7@gmail.com>
2023-10-17 14:52:05 -0400

    added id: number and type of Bingo Board

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,8 +1,17 @@
 export type Business = {
+  id: number;
   name: string;
   address: string;
 };
 
 export type Businesses = {
   [key: string]: Business[];
+};
+
+export type BingoBoard = {
+  B: Business[];
+  I: Business[];
+  N: Business[];
+  G: Business[];
+  O: Business[];
 };

cb84f1d3b94cafda52a7e1c438a921e62438fa19
Matt Vinall <matt.vinall7@gmail.com>
2023-10-17 20:53:53 -0400

    update classes

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,15 @@ import { businesses } from "./constants";
 
 const Home: React.FC = () => {
   return (
-    <main>
-      <section className="container mx-auto px-4 flex flex-col md:flex-row md:space-x-4">
-        <div className="flex-1 my-8">
-          <BingoBoard businesses={businesses} />
-        </div>
-      </section>
-    </main>
+    <>
+      <main className="min-h-[120vh] md:min-h-[100vh]">
+        <section className="mx-auto px-4 flex flex-col md:flex-row md:space-x-4">
+          <div className="flex-1 my-8">
+            <BingoBoard businesses={businesses} />
+          </div>
+        </section>
+      </main>
+    </>
   );
 };
 

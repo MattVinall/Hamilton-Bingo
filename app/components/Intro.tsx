b83eb84c7f5f590fe57ab650a34369c95dbcfc6e
Matt Vinall <matt.vinall7@gmail.com>
2023-10-18 12:42:21 -0400

    update iframe attributes to compile

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -10,7 +10,7 @@ export const Intro = () => {
 
     return (
         <section className="h-[85vh] flex flex-col justify-center items-center">
-             <iframe className="mb-16" width="560" height="315" src="https://www.youtube.com/embed/1w6-BLOt2os?si=WhLH66sXBe4niVyl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe className="mb-16" width="560" height="315" src="https://www.youtube.com/embed/1w6-BLOt2os?si=WhLH66sXBe4niVyl" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen loading="lazy"></iframe>            
             <div className="container mx-auto ">
                 <p className="text-black font-bold text-medium md:w-[80%] md:mx-auto">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem assumenda vero quibusdam illum voluptatum iste harum sequi a modi dignissimos nulla temporibus dolore cumque, velit tempora? Libero, rerum perspiciatis cumque, porro voluptate dicta sint esse perferendis ipsa similique aliquam laudantium fuga nemo, in optio enim fugiat? Tempore quos rerum odit porro beatae magnam dolorem soluta reiciendis quia nulla optio debitis illum, non sed totam at dolore cumque eos libero distinctio adipisci maxime accusantium atque?

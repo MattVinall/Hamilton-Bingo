da130e78c869a247380607334a91e773066405ab
Matt Vinall <matt.vinall7@gmail.com>
2023-10-18 11:26:23 -0400

    Intro component with lorem text

---
+++ app/components/Intro.tsx
@@ -0,0 +1,14 @@
+export const Intro = () => {
+    return (
+        <section className="h-[85vh] flex flex-col justify-center items-center">
+            <div className="container mx-auto ">
+                <p className="text-black font-bold text-medium md:w-[80%] md:mx-auto">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem assumenda vero quibusdam illum voluptatum iste harum sequi a modi dignissimos nulla temporibus dolore cumque, velit tempora? Libero, rerum perspiciatis cumque, porro voluptate dicta sint esse perferendis ipsa similique aliquam laudantium fuga nemo, in optio enim fugiat? Tempore quos rerum odit porro beatae magnam dolorem soluta reiciendis quia nulla optio debitis illum, non sed totam at dolore cumque eos libero distinctio adipisci maxime accusantium atque?
+                </p>
+                <p className="text-black font-bold text-medium md:w-[80%] md:mx-auto mt-8">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem assumenda vero quibusdam illum voluptatum iste harum sequi a modi dignissimos nulla temporibus dolore cumque, velit tempora? Libero, rerum perspiciatis cumque, porro voluptate dicta sint esse perferendis ipsa similique aliquam laudantium fuga nemo, in optio enim fugiat? Tempore quos rerum odit porro beatae magnam dolorem soluta reiciendis quia nulla optio debitis illum, non sed totam at dolore cumque eos libero distinctio adipisci maxime accusantium atque?
+                </p>
+            </div>
+        </section>
+    );
+}

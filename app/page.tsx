98524a4b9d1c4b1aebe97d6bb11f63913b6bbf49
Matt Vinall <matt.vinall7@gmail.com>
2023-10-18 11:25:23 -0400

    import Navbar and Intro component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@
 import React from "react";
 import BingoBoard from "./components/BingoBoard";
 import { businesses } from "./constants";
+import Navbar from "./components/Navbar";
+import { Intro } from "./components/Intro";
 
 const Home: React.FC = () => {
   return (
     <>
+      <Navbar />
+      <Intro />
       <main className="min-h-[120vh] md:min-h-[100vh]">
         <section className="mx-auto px-4 flex flex-col md:flex-row md:space-x-4">
           <div className="flex-1 my-8">

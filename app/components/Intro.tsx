af6ad65c35be884bc5229102fd47be5ff89fdc28
Matt Vinall <matt.vinall7@gmail.com>
2023-10-18 12:46:56 -0400

    fix iframe attributes again oopsies

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -10,8 +10,7 @@ export const Intro = () => {
 
     return (
         <section className="h-[85vh] flex flex-col justify-center items-center">
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/y6120QOlsfU?si=A7g3AQfFnQgylwyr" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen loading="lazy"></iframe>
-            <div className="container mx-auto ">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/y6120QOlsfU?si=A7g3AQfFnQgylwyr" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen loading="lazy"></iframe>            <div className="container mx-auto ">
                 <p className="text-black font-bold text-medium md:w-[80%] md:mx-auto">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem assumenda vero quibusdam illum voluptatum iste harum sequi a modi dignissimos nulla temporibus dolore cumque, velit tempora? Libero, rerum perspiciatis cumque, porro voluptate dicta sint esse perferendis ipsa similique aliquam laudantium fuga nemo, in optio enim fugiat? Tempore quos rerum odit porro beatae magnam dolorem soluta reiciendis quia nulla optio debitis illum, non sed totam at dolore cumque eos libero distinctio adipisci maxime accusantium atque?
                 </p>

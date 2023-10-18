6fd8725dab0de5ff2ff6dfcd97b9baec6ae27584
Matt Vinall <matt.vinall7@gmail.com>
2023-10-18 11:54:15 -0400

    update to font size

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className='container mx-auto flex justify-between items-center mb-6'>
                 <Image src={Logo} alt="Logo" width={150} height={100} />
                 <div className='flex flex-col'>
-                    <h1 className="text-black font-bold text-4xl">
+                    <h1 className="text-black font-extrabold text-5xl">
                         Hamilton Bingo
                     </h1>
                     <p className="font-medium text-black">Andrew De Marzo, REALTOR®</p>

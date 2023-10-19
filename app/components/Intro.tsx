19bfdaec82f44b5a1e8be26c967a1d14bed199ee
Matt Vinall <matt.vinall7@gmail.com>
2023-10-19 14:38:01 -0400

    jump to print

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -8,6 +8,14 @@ export const Intro = () => {
         }
     };
 
+    const handleJumpToPrint = (event: React.MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault();
+        const print = document.getElementById('print');
+        if (print) {
+            print.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <section id="intro" className="h-[85vh] flex flex-col justify-center items-center">
             <iframe className="mb-12" width="560" height="315" src="https://www.youtube.com/embed/y6120QOlsfU?si=A7g3AQfFnQgylwyr" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen loading="lazy"></iframe>
@@ -19,7 +27,8 @@ export const Intro = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem assumenda vero quibusdam illum voluptatum iste harum sequi a modi dignissimos nulla temporibus dolore cumque, velit tempora? Libero, rerum perspiciatis cumque, porro voluptate dicta sint esse perferendis ipsa similique aliquam laudantium fuga nemo, in optio enim fugiat? Tempore quos rerum odit porro beatae magnam dolorem soluta reiciendis quia nulla optio debitis illum, non sed totam at dolore cumque eos libero distinctio adipisci maxime accusantium atque?
                 </p>
                 <div className="container mx-auto my-12 text-center">
-                    <a href="#bingoBoard" onClick={handleJumpToBingo} className="mt-8 bg-black text-white font-bold py-4 px-6 rounded-full">Jump to Bingo</a>
+                    <a href="#bingoBoard" onClick={handleJumpToBingo} className="mt-8 text-white font-bold py-4 px-6 rounded-full">Jump to Bingo</a>
+                    <a href="#print" onClick={handleJumpToPrint} className="mt-8 ml-8 text-white font-bold py-4 px-6 rounded-full">Print Bingo Board</a>
                 </div>
             </div>
         </section>

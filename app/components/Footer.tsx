607cd1f4e1815120f48fb40f6489c7d448a5502c
Matt Vinall <matt.vinall7@gmail.com>
2023-10-20 17:31:48 -0400

    footer margin top fix for desktop

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="border border-t-4 border-black 4px py-8">
             <div className="mx-4 md:container md:mx-auto flex flex-col md:flex-row items-center md:justify-between">
                 <p className="text-black font-bold">&copy; {new Date().getFullYear()} - All rights reserved</p>
-                <p className="text-black font-bold mt-4">Developed by: <a className="link" target="_blank" href="https://mattvinall.dev">Matt Vinall</a></p>
+                <p className="text-black font-bold mt-4 md:m-0">Developed by: <a className="link" target="_blank" href="https://mattvinall.dev">Matt Vinall</a></p>
             </div>
         </footer>
     )

23624cf34a23d71e261df74545ec27b43bd86f01
Matt Vinall <matt.vinall7@gmail.com>
2023-10-19 23:16:55 -0400

    50% ifrrame width on medium + devices

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -18,7 +18,7 @@ export const Intro = () => {
 
     return (
         <section id="intro" className="xl:min-h-[140vh] 2xl:min-h-[85vh] flex flex-col justify-center items-center">
-            <iframe className="w-[95%] mt-12 md:w-[80%] mb-12" width="560" height="315" src="https://www.youtube.com/embed/y6120QOlsfU?si=A7g3AQfFnQgylwyr" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen loading="lazy"></iframe>
+            <iframe className="w-[95%] mt-12 md:w-[50%] mb-12" width="560" height="315" src="https://www.youtube.com/embed/y6120QOlsfU?si=A7g3AQfFnQgylwyr" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen loading="lazy"></iframe>
             <div className="container px-4 md:mx-auto">
                 <p className="text-black font-medium text-medium md:w-[80%] md:mx-auto">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rem assumenda vero quibusdam illum voluptatum iste harum sequi a modi dignissimos nulla temporibus dolore cumque, velit tempora? Libero, rerum perspiciatis cumque, porro voluptate dicta sint esse perferendis ipsa similique aliquam laudantium fuga nemo, in optio enim fugiat? Tempore quos rerum odit porro beatae magnam dolorem soluta reiciendis quia nulla optio debitis illum, non sed totam at dolore cumque eos libero distinctio adipisci maxime accusantium atque?

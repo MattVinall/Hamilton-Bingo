7e823d69adc40b0da32b1f25b5eaff2c35c59425
Matt Vinall <matt.vinall7@gmail.com>
2023-10-19 21:04:59 -0400

    flex column on mobile

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
         <footer className="border border-t-4 border-black 4px py-8">
-            <div className="container mx-auto flex justify-between">
+            <div className="mx-4 md:container md:mx-auto flex flex-col items-center md:justify-between">
                 <p className="text-black font-bold">&copy; {new Date().getFullYear()} - All rights reserved</p>
-                <p className="text-black font-bold">Developed by: <a className="link" target="_blank" href="https://mattvinall.dev">Matt Vinall</a></p>
+                <p className="text-black font-bold mt-4">Developed by: <a className="link" target="_blank" href="https://mattvinall.dev">Matt Vinall</a></p>
             </div>
         </footer>
     )

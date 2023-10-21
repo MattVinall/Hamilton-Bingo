7c9f614a4833b7afc3aad2c7432d1b25a8397eb1
Matt Vinall <matt.vinall7@gmail.com>
2023-10-20 20:59:31 -0400

    remove addresses, fix gap/margin, etc...

--- app/components/BingoBoard.tsx
+++ app/components/BingoBoard.tsx
@@ -34,16 +34,15 @@ const BingoBoard: React.FC<Props> = ({ businesses }) => {
 
     return (
         <>
-            <div id="bingoBoard" className="md:rounded-lg p-2 md:p-4 min-h-[105vh] sm:min-h-[105vh] md:min-h-[100vh] xl:min-h-[90vh]! 2xl:min-h-[90vh]">
-                <p className="font-bold text-md mx-4 text-center mb-4 text-black">Presented by Andrew De Marzo, REALTOR®</p>
-                <div className="grid grid-cols-5 gap-1">
+            <div id="bingoBoard" className="md:rounded-lg p-2 md:p-4 min-h-[80vh] sm:min-h-[80vh] md:min-h-[80vh] xl:min-h-[90vh]! 2xl:min-h-[90vh]">
+                <p className="font-bold text-md mx-4 text-center md:m-0 mb-4 text-black">Presented by Andrew De Marzo, REALTOR®</p>
+                <div className="grid grid-cols-5 gap-0.5 md:gap-1">
                     {Object.keys(bingoBoard).map((key, index) => (
                         <div key={key + index} className="col-span-1">
-                            <h3 className="text-5xl md:text-7xl text-center text-black font-bold mb-2">{key}</h3>
+                            <h3 className="text-5xl md:text-7xl text-center text-black mb-2">{key}</h3>
                             {bingoBoard[key as keyof BingoBoard].map((business: Business) => (
-                                <div key={`${key}-${business.id}`} className="card w-full flex flex-col items-center justify-center h-[25%] md:h-[40%] px-2 md:px-2 text-xs my-2 md:my:4 bg-navy">
+                                <div key={`${key}-${business.id}`} className="card w-full flex flex-col items-center justify-center h-[25%] md:h-[50%] px-2 md:px-2 text-xs my-1 md:my:4 bg-navy">
                                     <h4 className="text-xs md:text-sm lg:text-lg text-center font-bold mb-3 text-white">{business.name}</h4>
-                                    <p className="text-center text-xs md:text-base lg:text-lg text-white">{business.address}</p>
                                 </div>
                             ))}
                         </div>
@@ -51,7 +50,7 @@ const BingoBoard: React.FC<Props> = ({ businesses }) => {
                 </div>
             </div>
             <div className="text-center">
-                <button onClick={handleRefresh} className="mt-12 bg-navy text-white rounded-lg py-4 px-12 hover:background-[#AB804D]">Refresh</button>
+                <button onClick={handleRefresh} className="mt-4 md:mt-12 bg-navy text-white rounded-lg py-4 px-12 hover:background-[#AB804D]">Refresh</button>
             </div>
         </>
     );

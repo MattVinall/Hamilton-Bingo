13b4b40d341f051ee957bc989eacf788b4042107
Matt Vinall <matt.vinall7@gmail.com>
2023-10-20 21:02:07 -0400

    fixed gap/margin on desktop

--- app/components/BingoBoard.tsx
+++ app/components/BingoBoard.tsx
@@ -36,12 +36,12 @@ const BingoBoard: React.FC<Props> = ({ businesses }) => {
         <>
             <div id="bingoBoard" className="md:rounded-lg p-2 md:p-4 min-h-[80vh] sm:min-h-[80vh] md:min-h-[80vh] xl:min-h-[90vh]! 2xl:min-h-[90vh]">
                 <p className="font-bold text-md mx-4 text-center md:m-0 mb-4 text-black">Presented by Andrew De Marzo, REALTOR®</p>
-                <div className="grid grid-cols-5 gap-0.5 md:gap-1">
+                <div className="grid grid-cols-5 gap-0.5 md:gap-2">
                     {Object.keys(bingoBoard).map((key, index) => (
                         <div key={key + index} className="col-span-1">
                             <h3 className="text-5xl md:text-7xl text-center text-black mb-2">{key}</h3>
                             {bingoBoard[key as keyof BingoBoard].map((business: Business) => (
-                                <div key={`${key}-${business.id}`} className="card w-full flex flex-col items-center justify-center h-[25%] md:h-[50%] px-2 md:px-2 text-xs my-1 md:my:4 bg-navy">
+                                <div key={`${key}-${business.id}`} className="card w-full flex flex-col items-center justify-center h-[25%] md:h-[50%] px-2 md:px-2 text-xs my-1 md:my-2 md:my:4 bg-navy">
                                     <h4 className="text-xs md:text-sm lg:text-lg text-center font-bold mb-3 text-white">{business.name}</h4>
                                 </div>
                             ))}

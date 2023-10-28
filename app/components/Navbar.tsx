984556bc514aa52ff998ee791d82e97ee045eead
Matt Vinall <matt.vinall7@gmail.com>
2023-10-27 23:18:03 -0400

    wrapped images in figure

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,10 +6,13 @@ function Navbar() {
     return (
         <header className='bg-cream my-6 border-black border-b-4 md:min-h-[15vh]'>
             <nav className='container mx-auto flex flex-col md:flex-row justify-around lg:justify-between items-center mb-6'>
-                <div className="flex flex-col">
-                    <Image src={Logo} alt="Logo" width={150} height={100} />
-
-                    <Image className="my-4" src={Logo2} alt="Logo" width={150} height={100} />
+                <div className="flex flex-col items-center">
+                    <figure>
+                        <Image src={Logo} alt="Logo" width={150} height={100} />
+                    </figure>
+                    <figure>
+                        <Image className="my-4 w-[200px] h-[120px] " src={Logo2} alt="Logo" width={150} height={150} />
+                    </figure>
                 </div>
                 <div className='flex flex-col'>
                     <h1 className="text-black font-extrabold text-5xl bingo-title text-center md:text-left">
